562593bfbc511923e8c3848c3af331483ccf273c
Teck wu Ong <ongteckwu@hotmail.com>
2014-07-18 00:18:36 +0800

    Very cool already

--- app/assets/javascripts/teckwu.js
+++ app/assets/javascripts/teckwu.js
@@ -1,6 +1,92 @@
 
 $(document).ready(function() {
   var s = skrollr.init()
+  //word cloud
+  var word_cloud_list = [["Javascript", 22],
+                         ["Ruby", 20],
+                         ["Python", 25],
+                         ["Selenium", 6],
+                         ["Pygame", 16],
+                         ["SpriteBuilder", 10],
+                         ["Algorithms", 7],
+                         ["Web Design", 18],
+                         ["Web Frameworks", 12],
+                         ["GAE", 10],
+                         ["Django", 16],
+                         ["Rails", 20],
+                         ["BDD", 10],
+                         ["TDD", 10],
+                         ["Rspec", 15],
+                         ["JQuery", 17],
+                         ["JQuery-UI", 15],
+                         ["Skrollr.js", 5],
+                         ["CSS", 10],
+                         ["HTML", 15],
+                         ["HAML", 12],
+                         ["Cucumber", 10],
+                         ["Objective C", 20],
+                         ["iOS", 15],
+                         ["XCode", 12],
+                         ["Databases", 12],
+                         ["Machine Learning", 13],
+                         ["Artificial Intelligence", 8],
+                         ["Unittest", 5],
+                         ["Rapid Prototyping", 6],
+                         ["C++", 12],
+                         ["C", 10],
+                         ["Git", 12]];
+
+  var my_canvas = document.getElementById("wordcloud");
+  my_canvas.width = 1000;
+  my_canvas.height = 600;
+  //whether cloud is out already
+  var wordcloud_out = false;
+  var scrolled = false;
+  var progress_bar_100 = $("body").height() - $(window).height();
+  $(document).on("scroll", document, function() {
+    if (!scrolled) {
+      $(".introp.progressbar").css("left", "48.8%");
+      scrolled = !scrolled;
+    }
+    var from_top = $("#wordcloud").offset().top - $(document).scrollTop();
+    var bar_percentage = ($(document).scrollTop()/progress_bar_100) * 100;
+
+    //round to 100 if hit 101
+    if (bar_percentage < 0) {
+      bar_percentage = 0;
+    }
+    else if (bar_percentage > 100) {
+      bar_percentage = 100;
+    }
+    //String it
+    bar_percentage = Math.round(bar_percentage);
+    if (bar_percentage === 100) {
+      $(".introp.progressbar").css("left", "48.2%");
+      scrolled = !scrolled;
+    }
+    bar_percentage = bar_percentage + "%"
+    $(".introp.progressbar").html(bar_percentage);
+
+    if ((from_top < 300 && from_top > -300) 
+          && !wordcloud_out) { 
+      WordCloud(my_canvas, 
+                {list: word_cloud_list,
+                 fontFamily: "Futura",
+                 weightFactor: 5,
+                 shape: "square",
+                 minSize: 10,
+                 rotateRatio: 0.1,
+                 wait: 30,
+                 backgroundColor : "#F0F8FF"});
+      wordcloud_out = !wordcloud_out;
+   }
+
+   if ((from_top < -700 || from_top > 700)
+        && wordcloud_out) {
+    my_canvas.width = 1000;
+    wordcloud_out = !wordcloud_out;
+   }
+  });
   var content_subclasses = {"about": {"color_start": "#363B7D",
                                       "color_hover": "#1D2060",
                                       "color_content":"#F0F8FF",
@@ -98,12 +184,14 @@ $(document).ready(function() {
                                               $(".cloud.x1").show()
                                                             .transition({scale: [3, 3], duration: 1500});
                                               cache_div2.show()
-                                                        .transition({scale: [2.5, 6],
+                                                        .transition({scale: [2.5, 10],
                                                                      delay: 200,
                                                                      duration: 1000},
                                                                      complete = function() { 
                                                                       //allow scrolling
                                                                       $('body').css("overflow", "auto")
+                                                                      //reveal progressbar percentage
+                                                                      $('.introp.progressbar').show();
                                                                      }
                                                                     );
                                              }
